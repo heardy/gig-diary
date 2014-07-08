d82ef4f55ada8ae4d3279009a92f54f3e8d1e359
Kevin Heard <kevin.heard@massiveinteractive.com>
2014-07-08 16:44:52 +1000

    cant remember if this was important

--- js/gig-diary.js
+++ js/gig-diary.js
@@ -1,13 +1,13 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-	console.log("dsf");
-	$scope.yourName = 'Angular';
+	// console.log("dsf");
+	// $scope.name = 'Angular';
 
 	$http.get('./data/concerts.json')
-	.success(function(a){
-		console.log("success");
-		console.log(a);
-		$scope.concerts = a;
+	.success(function(concerts){
+		// console.log("success");
+		// console.log(concerts);
+		$scope.concerts = concerts;
 	});
 }]);
